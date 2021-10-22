c5ca224e047fdea419d2250bf1955c6e082bc659
Abdul Mufeez <abdul_mufeez@outlook.com>
2021-10-22 22:06:05 +0500

    Successfully created first migrations with custom postgresql database

--- ChatApp/Data/AppDbContext.cs
+++ ChatApp/Data/AppDbContext.cs
@@ -1,8 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using ChatApp.Models;
 
 namespace ChatApp.Data {
-    public class AppDbContext : DbContext {
+    public class AppDbContext : IdentityDbContext<User> {           //Getting help from identityclass with user
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-
+        public DbSet<ChatApp.Models.Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
     }
 }
--- ChatApp/Startup.cs
+++ ChatApp/Startup.cs
@@ -1,6 +1,11 @@
+using ChatApp.Data;
+using ChatApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,18 +17,27 @@ namespace ChatApp
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(option => option.EnableEndpointRouting = false);              //for Model-view-controller
+            services.AddDbContext<AppDbContext>(p => p.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));   //telling database to configure database using AppDbContext
+            services.AddIdentity<User, IdentityRole>()                                      //Adding users as user role using IdentityRole
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {            
             app.UseDeveloperExceptionPage();
-            app.UseStaticFiles();
+            app.UseStaticFiles();                   //for using CSS files 
 
             app.UseMvcWithDefaultRoute();           //for default routing
             // app.UseEndpoints(endpoints =>
